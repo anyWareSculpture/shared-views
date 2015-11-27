6d14ffc4b683927a876f1cc897e81f02f72d74f2
Marius Kintel <marius@kintel.net>
2015-11-26 21:34:22 -0500

    Added loop frequency parameter, support fadeIn/out as separate methods, support overriding parameter on play/stop

--- src/audio-api.js
+++ src/audio-api.js
@@ -8,8 +8,9 @@ const context = initContext();
 
 let isNode = false;
 
+// FIXME: Should we cache the buffer to allow reuse of buffers using the same URL?
 export class Sound {
-  constructor({ url, loop = false, fadeIn = 0, fadeOut = fadeIn, rate = 1, gain = 1, name = path.basename(url, '.wav') } = {}) {
+  constructor({ url, loop = false, fadeIn = 0, fadeOut = fadeIn, rate = 1, loopFreq = 0, gain = 1, name = path.basename(url, '.wav') } = {}) {
 
     assert(url);
 
@@ -19,6 +20,7 @@ export class Sound {
       fadeIn,
       fadeOut,
       rate,
+      loopFreq,
       gain
     };
     this.name = name;
@@ -58,35 +60,68 @@ export class Sound {
       });
   }
 
-  play() {
-    if (this.params.fadeIn > 0) {
+  // Allows overriding parameters
+  play(parameters = {}) {
+    const params = Object.assign({}, this.params, parameters);
+    if (params.fadeIn > 0) {
       this.gain.gain.setValueAtTime(0, context.currentTime);
-      this.gain.gain.linearRampToValueAtTime(this.params.gain, context.currentTime + this.params.fadeIn);
+      this.gain.gain.linearRampToValueAtTime(params.gain, context.currentTime + params.fadeIn);
     }
 
     this.source = context.createBufferSource();
     this.source.buffer = this.buffer;
-    this.source.loop = this.params.loop;
-    if (this.params.rate != 1) this.source.playbackRate.value = this.params.rate;
-    if (this.params.gain != 1) this.gain.gain.value = this.params.gain;
+    this.source.loop = params.loop;
+    if (params.rate != 1) this.source.playbackRate.value = params.rate;
+    this.source.loopEnd = params.loopFreq === 0 ? 0 : 1/params.loopFreq;
+    if (params.gain != 1) this.gain.gain.value = params.gain;
     this.source.connect(this.head);
     if (isNode) this.gain.connect(context.destination);
     this.source.start(context.currentTime);
   }
 
-  stop() {
-    if (this.params.fadeOut > 0) {
+  // Allows overriding parameters
+  stop(parameters = {}) {
+    const params = Object.assign({}, this.params, parameters);
+    if (params.fadeOut > 0) {
       var volume = this.gain.gain.value;
       this.gain.gain.cancelScheduledValues(context.currentTime);
       this.gain.gain.setValueAtTime(volume, context.currentTime);
-      this.gain.gain.linearRampToValueAtTime(0,context.currentTime + volume*this.params.fadeOut);
-      this.gain.gain.setValueAtTime(1, context.currentTime + volume*this.params.fadeOut);
-      if (this.source) this.source.stop(context.currentTime + volume*this.params.fadeOut);
+      this.gain.gain.linearRampToValueAtTime(0,context.currentTime + volume*params.fadeOut);
+      this.gain.gain.setValueAtTime(1, context.currentTime + volume*params.fadeOut);
+      if (this.source) this.source.stop(context.currentTime + volume*params.fadeOut);
     }
     else {
       if (this.source) this.source.stop();
     }
   }
+
+  // Must start first
+  fadeIn() {
+    if (this.params.fadeIn > 0) {
+      var volume = this.gain.gain.value;
+      this.gain.gain.cancelScheduledValues(context.currentTime);
+      this.gain.gain.setValueAtTime(volume, context.currentTime);
+      this.gain.gain.linearRampToValueAtTime(this.params.gain, context.currentTime + this.params.fadeIn);
+    }
+    else {
+      this.gain.gain.value = this.params.gain;
+    }
+  }
+
+  // Will wait <delay> seconds before starting to fade out
+  fadeOut({delay = 0} = {}) {
+    if (this.params.fadeOut > 0) {
+      var volume = this.gain.gain.value;
+      this.gain.gain.cancelScheduledValues(context.currentTime);
+      this.gain.gain.setValueAtTime(volume, context.currentTime);
+      this.gain.gain.setValueAtTime(volume, context.currentTime + delay);
+      this.gain.gain.linearRampToValueAtTime(0, context.currentTime + delay + volume*this.params.fadeOut);
+    }
+    else {
+      this.gain.gain.value = 0;
+    }
+  }
+  
 }
 
 /**
